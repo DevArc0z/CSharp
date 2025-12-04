0a0a7984cb556c2a01eeffcbb100a2a712eaa2ef
Meical <meical.clifford@student.pxl.be>
2025-12-04 09:16:31 +0100

    yahooooo

---
+++ DemoInputControls/DemoInputControls/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoInputControls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void doubleButton_Click(object sender, RoutedEventArgs e)
+        {
+            string textFromTextBox = inputTextBox.Text;
+
+            bool isANumber = int.TryParse(textFromTextBox, out int numberFromTextBox);
+
+            if (isANumber)
+            {
+                int doubledNumber = numberFromTextBox * 2;
+                inputTextBox.Text = doubledNumber.ToString();
+            }
+            else
+            {
+                inputTextBox.Text += inputTextBox.Text;
+            }
+
+            inputTextBox.Background = Brushes.White;
+        }
+
+        private void inputTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            textBox.Background = Brushes.Red;
+        }
+    }
+}
---
+++ Test/Test/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        static List<int> randomNumbers;
+        static void Main(string[] args)
+        {
+                randomNumbers.Add(17);
+                randomNumbers.Add(23);
+                randomNumbers.Add(41);
+
+                Console.WriteLine(randomNumbers.Count);
+            
+        }
+    }
+}
