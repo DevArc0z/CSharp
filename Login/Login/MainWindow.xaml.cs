9d770fa9a8ad52dc7e1dbd64f8efe14881e8aa0a
Meical <meical.clifford@student.pxl.be>
2025-12-04 11:24:41 +0100

    FreddyFazbearHurhurhurhurhur

---
+++ Login/Login/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Login.Models;
+using Login.Services;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Schema;
+
+namespace Login
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string inputUsername = InputUsernameTextBox.Text;
+            string inputPassword = InputWachtwoordBox.Password;
+            if (UserManager.TryLogin(inputUsername, inputPassword))
+            {
+                ShowInfo("Inloggen gelukt!", Brushes.Green);
+                ClearScreen();
+            }
+            else
+            {
+                ShowInfo("Inloggen mislukt: verkeerde gebruikersnaam en of wachtwoord.", Brushes.Red);
+                ClearScreen();
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string inputUsername = InputUsernameTextBox.Text;
+            string inputPassword = InputWachtwoordBox.Password;
+
+            if (UserManager.Register(inputUsername, inputPassword))
+            {
+
+                ShowInfo("Registratie gelukt!", Brushes.Green);
+                ClearScreen();
+            }
+            else
+            {
+                ShowInfo("Registratie mislukt: gebruikersnaam bestaat al.", Brushes.Red);
+            }
+        }
+
+        private void ClearScreen()
+        {
+            InputUsernameTextBox.Clear();
+            InputWachtwoordBox.Clear();
+        }
+
+        private void ShowInfo(string info, SolidColorBrush color)
+        {
+            infoTextBlock.Text = info;
+            infoTextBlock.Foreground = color;
+        }
+    }
+}
---
+++ Login/Login/Services/UserManager.cs
@@ -0,0 +1,51 @@
+﻿using Login.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login.Services
+{
+    internal class UserManager
+    {
+        private static List<Registration> _users = new List<Registration>();
+
+        public static bool Register(string username, string password)
+        {
+            foreach(Registration user in _users)
+            {
+                if(user.UserName.Equals(username))
+                {
+                    return false;
+                }
+            }
+            Registration newUser = new Registration(username, HashPassword(password));
+            _users.Add(newUser);
+            return true;
+
+        }
+
+        public static bool TryLogin(string username, string password)
+        {
+            foreach(Registration user in _users)
+            {
+                if(user.UserName.Equals(username) && user.Password.Equals(HashPassword(password)))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private static string HashPassword(string password)
+        {
+            using (var sha = SHA256.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(password);
+                byte[] hash = sha.ComputeHash(bytes);
+                return Convert.ToBase64String(hash);
+            }
+        }
+    }
+}
