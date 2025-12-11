04c9e02dced4ac648fa99705739bb72da413939c
Meical <meical.clifford@student.pxl.be>
2025-12-11 11:23:15 +0100

    JAPAAAAAAN

---
+++ SliderProject/SliderProject/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SliderProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EllipseSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Japan.Width = EllipseSlider.Value;
+            Japan.Height = EllipseSlider.Value;
+        }
+    }
+}
